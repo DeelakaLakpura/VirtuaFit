68dbb12f5c5cf7a5e0d0bf6f85e626ab399edf93
Deelaka <Deelaka@DESKTOP-JCJ5TEN>
2023-11-24 14:23:00 +0530

    initial Commit

---
+++ app/src/main/java/com/deelaka/virtuafit/Authentication/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.deelaka.virtuafit.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.deelaka.virtuafit.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
