f7f37fc549b9fb8192771999b968cae1db3af2cc
Deelaka <Deelaka@DESKTOP-JCJ5TEN>
2023-11-25 17:31:12 +0530

    initial Commit

--- app/src/main/java/com/deelaka/virtuafit/SplashScreen/SplashActivity.java
+++ app/src/main/java/com/deelaka/virtuafit/SplashScreen/SplashActivity.java
@@ -2,18 +2,24 @@ package com.deelaka.virtuafit.SplashScreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
+import com.deelaka.virtuafit.MainActivity;
 import com.deelaka.virtuafit.R;
 import com.google.ar.core.ArCoreApk;
 
 public class SplashActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
 
     }
 
