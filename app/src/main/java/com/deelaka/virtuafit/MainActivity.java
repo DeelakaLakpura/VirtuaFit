f31550242c19b3ed05d4084fe569c4a237052266
Deelaka Lakpura <deelaka.lakpura94@gmail.com>
2023-12-10 14:40:40 +0530

    initial commit

--- app/src/main/java/com/deelaka/virtuafit/MainActivity.java
+++ app/src/main/java/com/deelaka/virtuafit/MainActivity.java
@@ -1,8 +1,14 @@
 package com.deelaka.virtuafit;
 
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import com.deelaka.virtuafit.Util.CameraPermissionHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +18,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this);
+            return;
+        }
+
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(
+                   this,
+                    getString(R.string.camera_permision_required),
+                    Toast.LENGTH_LONG
+            ).show();
+
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                CameraPermissionHelper.launchPermissionSettings(this);
+            }
+            //  this.finish();
+        }
+    }
+
 }
--- app/src/main/java/com/deelaka/virtuafit/SplashScreen/SplashActivity.java
+++ app/src/main/java/com/deelaka/virtuafit/SplashScreen/SplashActivity.java
@@ -22,6 +22,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
 
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
